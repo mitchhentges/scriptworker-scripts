e908bdfba997205c5ec0b17b2ac38f7687ac7584
Mihai Tabara <mtabara@mozilla.com>
2019-10-22 01:18:18 +0300

    Fix balrogscript too

--- balrogscript/setup.py
+++ balrogscript/setup.py
@@ -1,10 +1,6 @@
 import os
 from setuptools import setup, find_packages
 
-# We allow commented lines in this file
-project_dir = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(project_dir, 'requirements/base.in')) as f:
-    install_requires = [line.rstrip('\n') for line in f if not line.startswith('#')]
 
 with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "version.txt")) as f:
     version = f.read().rstrip()
@@ -29,7 +25,17 @@ setup(
         ],
     },
     license="MPL2",
-    install_requires=install_requires,
+    install_requires=[
+        'balrogclient',
+        'idna',
+        'ipaddress',
+        'pyasn1',
+        'mar',
+        'python-dateutil',
+        'redo',
+        'requests',
+        'scriptworker',
+    ],
     classifiers=(
         'Intended Audience :: Developers',
         'Natural Language :: English',
