163992d4afc23e02a7e20faaf30a93cb1c5eb747
Mihai Tabara <mtabara@mozilla.com>
2019-10-22 00:37:53 +0300

    Another attempt. Fried brains

--- bouncerscript/setup.py
+++ bouncerscript/setup.py
@@ -7,10 +7,6 @@ project_dir = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(project_dir, 'version.txt')) as f:
     version = f.read().rstrip()
 
-# We allow commented lines in this file
-with open(os.path.join(project_dir, 'requirements/base.in')) as f:
-    requirements = [line.rstrip('\n') for line in f if not line.startswith('#')]
-
 
 setup(
     name='bouncerscript',
@@ -31,7 +27,10 @@ setup(
         ],
     },
     license='MPL2',
-    install_requires=requirements,
+    install_requires=[
+        "scriptworker",
+        "mozilla-version",
+    ],
     classifiers=(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
